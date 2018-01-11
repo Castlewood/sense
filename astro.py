76bcee9852ec04cf3f7f9457f4320981005a6502
castlewood <gavin.hewins@googlemail.com>
2018-01-11 13:05:40 +0000

    add raw accelerometer data

--- astro.py
+++ astro.py
@@ -13,12 +13,13 @@ def read_data():
     lastread=datetime.datetime.now()
     orientation = sense.get_orientation()
     compass_raw = sense.get_compass_raw()
-    data={'Orientation':orientation, 'Compass_Raw':compass_raw}
+    accelerometer_raw = sense.get_accelerometer_raw()
+    data={'Orientation':orientation, 'Compass_Raw':compass_raw, 'Accelerometer_raw':accelerometer_raw}
     return data
 
 with open( "pitch_data.csv" , "a" ) as data_file:
     writer=csv.writer(data_file,delimiter=',',quotechar=' ',quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['pitch','roll','yaw','mag_x','mag_y','mag_z','timestamp'])
+    writer.writerow(['pitch','roll','yaw','mag_x','mag_y','mag_z','accel_x','accel_y','accel_z','timestamp'])
 
     while True:
         timestamp=datetime.datetime.now()
@@ -27,7 +28,10 @@ with open( "pitch_data.csv" , "a" ) as data_file:
             data=read_data()
             orientation=data['Orientation']
             compass_raw=data['Compass_Raw']
-            writer.writerow([str(orientation['pitch']),str(orientation['roll']),str(orientation['yaw']), str(compass_raw['x']),str(compass_raw['y']),str(compass_raw['x']), str(timestamp)])
+            accel_raw=data['Accelerometer_raw']
+            writer.writerow([str(orientation['pitch']),str(orientation['roll']),str(orientation['yaw']),
+                             str(compass_raw['x']),str(compass_raw['y']),str(compass_raw['x']),
+                             str(accel_raw['x']),str(accel_raw['y']),str(accel_raw['x']), str(timestamp)])
             data_file.flush()
             print(data)
 
