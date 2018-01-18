619336c9cd6906091c7a90b7c03fdeb1bb9f54f5
castlewood <gavin.hewins@googlemail.com>
2018-01-18 13:10:30 +0000

    added display

--- astro.py
+++ astro.py
@@ -22,28 +22,30 @@ def read_data():
     data={'Humidity':humidity,'Temperature':temperature,'Pressure':pressure,'Orientation':orientation, 'Compass_Raw':compass_raw, 'Gyroscope_raw':gyroscope_raw, 'Accelerometer_raw':accelerometer_raw}
     return data
 
-with open( "pitch_data.csv" , "a" ) as data_file:
-    writer=csv.writer(data_file,delimiter=',',quotechar=' ',quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['humidity','temperature','pressure','pitch','roll','yaw','compass_x','compass_y','compass_z','gyroscope_x','gyroscope_y','gyroscope_z','accel_x','accel_y','accel_z','timestamp'])
 
-    while True:
-        timestamp=datetime.datetime.now()
-    
-        if (timestamp-lastread).total_seconds() > sampling_interval:        
-            data=read_data()
-            humidity=data['Humidity']
-            temperature=str(data['Temperature'])
-            pressure=str(data['Pressure'])
-            orientation=data['Orientation']
-            compass_raw=data['Compass_Raw']
-            gyroscope_raw=data['Gyroscope_raw']
-            accel_raw=data['Accelerometer_raw']
-            writer.writerow([humidity,temperature,pressure,str(orientation['pitch']),str(orientation['roll']),str(orientation['yaw']),
-                             str(compass_raw['x']),str(compass_raw['y']),str(compass_raw['x']),
-                             str(gyroscope_raw['x']),str(gyroscope_raw['y']),str(gyroscope_raw['x']),
-                             str(accel_raw['x']),str(accel_raw['y']),str(accel_raw['x']), str(timestamp)])
-            data_file.flush()
-            print(data)
+def log():
+    with open( "pitch_data.csv" , "a" ) as data_file:
+        writer=csv.writer(data_file,delimiter=',',quotechar=' ',quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['humidity','temperature','pressure','pitch','roll','yaw','compass_x','compass_y','compass_z','gyroscope_x','gyroscope_y','gyroscope_z','accel_x','accel_y','accel_z','timestamp'])
+
+        while True:
+            timestamp=datetime.datetime.now()
+        
+            if (timestamp-lastread).total_seconds() > sampling_interval:        
+                data=read_data()
+                humidity=data['Humidity']
+                temperature=str(data['Temperature'])
+                pressure=str(data['Pressure'])
+                orientation=data['Orientation']
+                compass_raw=data['Compass_Raw']
+                gyroscope_raw=data['Gyroscope_raw']
+                accel_raw=data['Accelerometer_raw']
+                writer.writerow([humidity,temperature,pressure,str(orientation['pitch']),str(orientation['roll']),str(orientation['yaw']),
+                                 str(compass_raw['x']),str(compass_raw['y']),str(compass_raw['x']),
+                                 str(gyroscope_raw['x']),str(gyroscope_raw['y']),str(gyroscope_raw['x']),
+                                 str(accel_raw['x']),str(accel_raw['y']),str(accel_raw['x']), str(timestamp)])
+                data_file.flush()
+                print(data)
 
         
             
---
+++ clear.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.clear()
